aaa42c60fda2a0b60d0a817b00baad15ba43242a
Meriam <ayer.meryam@gmail.com>
2023-01-10 09:52:12 +0100

    second commit

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -100,7 +100,7 @@ export default class PokemonService {
     return new Promise(resolve => {  
       
      //resolve(this.pokemons.find(pokemon => id === pokemon.id));
-    
+    const test= 12;
     }); 
   }
   
